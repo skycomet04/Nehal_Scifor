2b7fe7fb00484844ae4897e920eab18f96cfc25b
skycomet04 <124564052+skycomet04@users.noreply.github.com>
2024-04-25 22:25:39 +0530

    Update urls.py

--- miniproject/youtubevideodownloader/firstdjango/urls.py
+++ miniproject/youtubevideodownloader/firstdjango/urls.py
@@ -3,6 +3,4 @@ from . import views
 urlpatterns=[
     path('', views.youtube, name='youtube'),
     path('emailsender',views.emailsender,name='emailsender'),
-    path('fetch_data',views.SaveData.as_view(),name='fetch_data'),
-    path('display_data',views.viewStock.as_view(),name='display_data')
 ]
