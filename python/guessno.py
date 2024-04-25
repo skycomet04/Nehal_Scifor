e7905979625000923e15cd3a5859c88eed5007a5
skycomet04 <124564052+skycomet04@users.noreply.github.com>
2024-04-26 00:21:42 +0530

    Delete python directory

--- python/guessno.py
+++
@@ -1,9 +0,0 @@
-def guess_no(user,comp_no,point):
-  if comp_no==user:
-    point+=1
-    ans=f"no is equal"
-  elif user>comp_no:
-    print(f"no is smaller than {user}")
-  else:
-    print(f"no is greater than {user}")
-  return point
