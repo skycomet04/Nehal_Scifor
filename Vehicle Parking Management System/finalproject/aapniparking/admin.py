f03604a2bd92d4d1a11165551f19e2c90b211513
skycomet04 <124564052+skycomet04@users.noreply.github.com>
2024-04-26 00:30:49 +0530

    admin.py

---
+++ Vehicle Parking Management System/finalproject/aapniparking/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Vehicle,ParkingBookSlot,ParkingSpace
+admin.site.register(Vehicle)
+admin.site.register(ParkingBookSlot)
+admin.site.register(ParkingSpace)
+
