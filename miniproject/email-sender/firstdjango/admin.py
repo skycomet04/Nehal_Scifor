837b620bad543afa90786263d27ece3c51f292e4
skycomet04 <124564052+skycomet04@users.noreply.github.com>
2024-04-25 21:13:46 +0530

    Create admin.py

---
+++ miniproject/email-sender/firstdjango/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from firstdjango.models import Emailsender ,StockData
+# Register your models here.
+admin.site.register(StockData)
+admin.site.register(Emailsender)
+
